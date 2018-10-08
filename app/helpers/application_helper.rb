c4a2b4ebf45abc217908444f39607c97e7dbdfa6
yamato48 <yamd1048@gmail.com>
2018-10-08 17:27:35 +0900

    bootswatch　ハンバーガーメニュー対応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def nav_item_active(path_name)
+    request.url.include?(path_name) ? 'list-group-item active' : 'nav-item'
+  end
+
+  def nav_user_signed
+    if user_signed_in?
+      link_to 'ユーザー', '#', class: nav_item_active('Users')
+    else
+      link_to 'ログイン', destroy_user_session, class: nav_item_active('Users')
+    end
+  end
 end
